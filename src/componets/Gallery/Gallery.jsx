b0e2b8ef01699cfe5d90c34374ae7d09f543c5ec
Kinotijoan <kinotijoan@gmail.com>
2024-05-29 20:41:56 +0300

    refactored moved fctionality to Gallery

--- src/componets/Gallery/Gallery.jsx
+++ src/componets/Gallery/Gallery.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
-import UploadImage from './UploadImage'
-import Display from './Display'
+import React from "react";
+import UploadImage from "./UploadImage";
+import Display from "./Display";
+import { getDatabase, ref as dbRef, set } from "firebase/database";
+import { storage } from "../../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
 
 const Gallery = () => {
+  const [value, setValue] = React.useState({
+    image: "",
+    text: "",
+  });
+
+  const [data, setData] = React.useState(null);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const uploadImage = async (e) => {
+    e.preventDefault();
+
+    if (value.image == null) {
+      return alert("Kindly Input image");
+    } else if (value.text == null) {
+      return alert("Kindly Input text");
+    } else if (value.image == null && value.text == null) {
+      return alert("Kindly Input image and text");
+    }
+    const imageRef = ref(storage, `images/ ${value.image.name + v4()}`);
+    await uploadBytes(imageRef, value.image)
+      .then(() => {
+        console.log("Image Uploaded");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    await getDownloadURL(imageRef).then((url) => {
+      console.log(url);
+      const db = getDatabase();
+      set(dbRef(db, "images/" + value.image.name + v4()), {
+        imageUrl: url,
+        text: value.text,
+      });
+    });
+  };
+
   return (
     <>
       <h2>DailyDumps</h2>
       <div className="gallery-container">
-        <UploadImage />
+        <UploadImage handleChange={handleChange} uploadImage={uploadImage} />
         <Display />
       </div>
     </>
   );
-}
-
-export default Gallery
+};
+export default Gallery;
--- src/componets/Gallery/UploadImage.jsx
+++ src/componets/Gallery/UploadImage.jsx
@@ -1,106 +1,40 @@
-import React from 'react'
-import { Upload } from 'lucide-react';
-import { getDatabase, ref as dbRef, set } from "firebase/database";
-import { storage } from '../../firebase';
-import {getDownloadURL, ref, uploadBytes} from 'firebase/storage'
-import { v4 } from 'uuid';
-import Display from './Display';
+import { Upload } from "lucide-react";
 
-const UploadImage = () => {
-  const[value, setValue] = React.useState({
-    image: '',
-    text: ''
-  })
-
-  const [data, setData] = React.useState(null);
-
-  const handleChange = (e) => {
-    e.preventDefault()
-    setValue({
-      ...value,
-      [e.target.name]: e.target.value
-    })
-  }
-
-
-
-  const uploadImage = (e) => {
-    e.preventDefault()
-    
-    if(value.image == null) {
-      return alert("Kindly Input image")
-    }
-    else if(value.text == null) {
-      return alert("Kindly Input text")
-    }
-    else if (value.image == null && value.text == null) {
-      return alert("Kindly Input image and text")
-    }
-    const imageRef = ref(storage, `images/ ${value.image.name + v4()}`);
-    uploadBytes(imageRef, value.image).then(() => {
-      console.log('Image Uploaded')
-    }).catch((error) => {
-      console.log(error)
-    });
-    getDownloadURL(imageRef).then((url) => {
-      console.log(url)
-      const db = getDatabase();
-      set(dbRef(db, 'images/' + value.image.name + v4()), {
-        imageUrl: url,
-        text: value.text
-      })
-    })
-
-    //  useEffect(() => {
-    //    const db = getDatabase();
-    //    get(child(ref(db, "images/"), file.name))
-    //      .then((snapshot) => {
-    //        if (snapshot.exists()) {
-    //          setData(snapshot.val());
-    //        } else {
-    //          console.log("No data available");
-    //        }
-    //      })
-    //      .catch((error) => {
-    //        console.error(error);
-    //      });
-    //  }, []);
-  }
-
+const UploadImage = ({ handleChange, uploadImage }) => {
   return (
-      <form action="">
-        <h4>Upload Image</h4>
-        <div className="input-container">
-          <div>
-            <label htmlFor="image">
-              <div className="image-input">
-                Upload
-                <Upload size={30} />
-              </div>
-            </label>
-            <input
-              id="image"
-              name="image"
-              onChange={handleChange}
-              type="file"
-            />
-          </div>
-          <div className="text-div">
-            <textarea
-              className="text-input"
-              placeholder="A little Recap ..."
-              onChange={handleChange}
-              name="text"
-              id=""
-            ></textarea>
-          </div>
+    <form action="">
+      <h4>Upload Image</h4>
+      <div className="input-container">
+        <div>
+          <label htmlFor="image">
+            <div className="image-input">
+              Upload
+              <Upload size={30} />
+            </div>
+          </label>
+          <input
+            id="image"
+            name="image"
+            onChange={() => handleChange}
+            type="file"
+          />
         </div>
-        <div className="button-container">
-          <button>Cancel</button>
-          <button onClick={uploadImage}>Upload</button>
+        <div className="text-div">
+          <textarea
+            className="text-input"
+            placeholder="A little Recap ..."
+            onChange={() => handleChange}
+            name="text"
+            id=""
+          ></textarea>
         </div>
-      </form>
+      </div>
+      <div className="button-container">
+        <button>Cancel</button>
+        <button onClick={uploadImage}>Upload</button>
+      </div>
+    </form>
   );
-}
+};
 
-export default UploadImage
+export default UploadImage;
